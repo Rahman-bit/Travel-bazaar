8c601214365664101af14710d894e42d58c5bdbc
Rahman-bit <rahaman77k@gmail.com>
2024-10-17 23:03:38 +0530

    updated router

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3500',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization', 
     credentials: true, 
   });
-  await app.listen(process.env.PORT || 5500);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
